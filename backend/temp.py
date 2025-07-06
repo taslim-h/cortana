8622065fe13eea260d7ef1f41d2fb42fddb240f4
Taslim Hossain <taslim.hossain.work@gmail.com>
2025-07-06 20:12:14 +0600

    minor changes

--- backend/temp.py
+++ backend/temp.py
@@ -4,6 +4,8 @@ import json
 with open(r"C:\Users\USER\Downloads\cortana-4cc9a-firebase-adminsdk-fbsvc-2b18bcc557.json") as f:
     json_data = f.read()
 
-encoded = base64.b64encode(json_data.encode()).decode()
 
-print(encoded)
+
+# encoded = base64.b64encode(json_data.encode()).decode()
+
+print(json_data)
